2f8dfebe100eb80d3ef17fb9c7ecb5a71abc8d5f
Victor McLean <vmvictorvm@gmail.com>
2016-11-23 10:23:40 -0500

    Imported navbar, search and about components into app.module.ts

--- app/app.module.ts
+++ app/app.module.ts
@@ -2,10 +2,13 @@ import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent }  from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { SearchComponent } from './components/search/search.component';
+import { AboutComponent } from './components/about/about.component';
 
 @NgModule({
   imports:      [ BrowserModule ],
-  declarations: [ AppComponent ],
+  declarations: [ AppComponent, NavbarComponent, SearchComponent, AboutComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
