b7672c6ae6b4f4e85c8bda78fcb2932aa85a8a4d
Victor McLean <vmvictorvm@gmail.com>
2016-11-23 09:50:47 -0500

    Added navbar, search and about components

---
+++ app/components/search/search.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  moduleId: module.id,
+  selector: 'my-search',
+  templateUrl: 'search.component.html',
+})
+export class SearchComponent  {  }
