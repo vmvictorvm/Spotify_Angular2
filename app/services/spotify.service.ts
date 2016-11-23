c6359204ea71364f81de8ec1524d1d1a408ee99a
Victor McLean <vmvictorvm@gmail.com>
2016-11-23 14:52:47 -0500

    Implemented getArtist method in spotify service

--- app/services/spotify.service.ts
+++ app/services/spotify.service.ts
@@ -13,5 +13,10 @@ export class SpotifyService {
         return this._http.get('https://api.spotify.com/v1/search?query=' + str + '&offset=0&limit=20&type=artist&market=US')
                          .map(res => res.json());
     }
+
+    getArtist(id:string) {
+        return this._http.get('https://api.spotify.com/v1/artists/'+ id)
+                         .map(res => res.json());
+    }
 }
 
