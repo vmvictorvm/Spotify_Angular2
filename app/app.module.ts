033e64288ca8752600adbe4770c46882f3b446cf
Victor McLean <vmvictorvm@gmail.com>
2016-11-24 19:37:55 -0500

    Fixed refresh page not loading issue

--- app/app.module.ts
+++ app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 
 import { AppComponent }  from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
@@ -15,6 +16,7 @@ import { routing } from './app.routing';
 @NgModule({
   imports:      [ BrowserModule, routing, FormsModule, HttpModule ],
   declarations: [ AppComponent, NavbarComponent, SearchComponent, AboutComponent, ArtistComponent, AlbumComponent ],
+  providers: [ {provide: LocationStrategy, useClass: HashLocationStrategy} ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
